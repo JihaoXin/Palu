9dc513c05a30335b9acbd8c60d8561db35509a3d
Jihao Xin <jihaoxin1998@gmail.com>
2025-09-04 15:32:02 +0000

    Simplyfi palu kvcache logic

--- kernel/palu_attention.py
+++ kernel/palu_attention.py
@@ -234,18 +234,18 @@ class LlamaPaluAttention(LlamaAttention):
             latent_dim = key_latents.shape[-1] // self.num_key_value_heads
             key_latents = key_latents.view(batch_size, seq_len, self.num_key_value_heads, latent_dim).transpose(1, 2) # [batch, heads, seq, latent_dim]
             _, key_latents_rope = apply_rotary_pos_emb(None, key_latents, cos[..., :latent_dim], sin[..., :latent_dim])  # [batch, heads, seq, latent_dim]
-            key_latents_rope = key_latents_rope.transpose(1, 2).reshape(batch_size, seq_len, -1) # [batch, seq, total_latent_k]
             value_latents = self.v_proj.project_to_latent(hidden_states)  # x@U: [batch, seq, total_latent_v]
             # K/V up projection
             if past_key_value is None:
                 self.print_once("past_key_value", f"😄😄😄😄😄😄😄😄😄😄😄No KV Cache")
+                key_latents_rope = key_latents_rope.transpose(1, 2).reshape(batch_size, seq_len, -1) # [batch, seq, total_latent_k]
                 key_states = self.k_proj.reconstruct(key_latents_rope).view(batch_size, seq_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
                 value_states = self.v_proj.reconstruct(value_latents).view(batch_size, seq_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
             elif past_key_value is not None: 
                 self.print_once("past_key_value", f"😄😄😄😄😄😄😄😄😄😄😄Using KV Cache;")
                 cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
-                key_latents_to_cache = key_latents_rope.view(batch_size, seq_len, self.num_groups, -1)  # [batch, seq, groups, group_rank_k]
-                value_latents_to_cache = value_latents.view(batch_size, seq_len, self.num_groups, -1)    # [batch, seq, groups, group_rank_v]
+                key_latents_to_cache = key_latents_rope.transpose(1, 2)  # [batch, seq, heads, latent_dim]
+                value_latents_to_cache = value_latents.view(batch_size, seq_len, self.num_key_value_heads, -1)    # [batch, seq, heads, latent_dim]
                 cached_key_latents_rope, cached_value_latents = past_key_value.update(key_latents_to_cache, value_latents_to_cache, self.layer_idx, cache_kwargs)
                 key_states = self.k_proj.reconstruct(cached_key_latents_rope.view(batch_size, seq_len, -1)).view(batch_size, seq_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
                 value_states = self.v_proj.reconstruct(cached_value_latents.view(batch_size, seq_len, -1)).view(batch_size, seq_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
